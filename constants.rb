e5de20a2dfb145470cf026348c23f8f4ce671871
Paul Codding <pcodding@gmail.com>
2012-07-30 01:30:09 -0500

    Fixed the incorrect brackets

--- constants.rb
+++ constants.rb
@@ -1,7 +1,7 @@
 module Constants
   FRAUDULENT_IP = "72.30.38.140"
-  FRAUDULENT_USERS = { "ari.kleitz","tom.dapp","pierre.difozzio","pradeep.roberts","muriel.fen" }
-  NORMAL_USERS = { "weem.ingles","kosmo.tyler","deb.summers","lea.cook","manhkiet.king","guru.segal","srikanth.stewart","arnault.geller","ross.milton","winona.corvino","ross.newburg","katie.kilpatrick","stan.delott","alfred.lowe","marcio.stewart","katherine.leutwyler","sylvia.smythe","heath.jasuja","leonard.teichgraeber","george.dvorak","ocey.arredondo","silvia.piccione","guru.krishnamurthy","alfred.kolli","hormazd.lopez","satish.sherman","guru.evans","nilesh.anniston","paolo.kiessel","julio.geller","manhkiet.fischetti","winona.mccoy","iris.nam","alastair.curry","jen.lang","al.fen","ruby.galos","nathan.jones","veronica.wei","ethan.florek","kosmo.guralnick","vikki.schube","gurudatt.koski","adina.leutwyler","agarwal.fen","gustavo.knowler","connie.nash","veronica.purdon","vic.cuche" }
+  FRAUDULENT_USERS = ["ari.kleitz","tom.dapp","pierre.difozzio","pradeep.roberts","muriel.fen"]
+  NORMAL_USERS = ["weem.ingles","kosmo.tyler","deb.summers","lea.cook","manhkiet.king","guru.segal","srikanth.stewart","arnault.geller","ross.milton","winona.corvino","ross.newburg","katie.kilpatrick","stan.delott","alfred.lowe","marcio.stewart","katherine.leutwyler","sylvia.smythe","heath.jasuja","leonard.teichgraeber","george.dvorak","ocey.arredondo","silvia.piccione","guru.krishnamurthy","alfred.kolli","hormazd.lopez","satish.sherman","guru.evans","nilesh.anniston","paolo.kiessel","julio.geller","manhkiet.fischetti","winona.mccoy","iris.nam","alastair.curry","jen.lang","al.fen","ruby.galos","nathan.jones","veronica.wei","ethan.florek","kosmo.guralnick","vikki.schube","gurudatt.koski","adina.leutwyler","agarwal.fen","gustavo.knowler","connie.nash","veronica.purdon","vic.cuche"]
   
   def load_fraudulent_user
     load_user :login_id => get_random_fraudulent_login_id
